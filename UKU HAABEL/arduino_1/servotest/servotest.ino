e86a357d6a5565740b4088a351fa35a6d86c04c5
Quower <81152957+Quower@users.noreply.github.com>
2023-01-27 18:20:55 +0200

    Add files via upload

--- UKU HAABEL/arduino_1/servotest/servotest.ino
+++ UKU HAABEL/arduino_1/servotest/servotest.ino
@@ -2,16 +2,34 @@
 #define servo 10
 #define pingPin 12
 #define echoPin 11
+#define mrb 6
+#define mrf 3
+#define mlf 5
+#define mlb 9
+#define calibratePin 8
+int walldistance = 20;
 
 Servo myservo;
-void setup() {
+void setup()
+{
     myservo.attach(servo);
     Serial.begin(250000);
+    pinMode(calibratePin, INPUT);
+    pinMode(mrf, INPUT);
+    pinMode(mrb, INPUT);
+    pinMode(mlf, INPUT);
+    pinMode(mlb, INPUT);
 }
-void loop() {
-    for(int i = 0; i<5;i = i+1) {
-        myservo.write(i*36);
-        delay(75);
+void loop()
+{
+    if (digitalRead(calibratePin) == HIGH)
+    {
+        analogWrite(mrb, 0);
+        analogWrite(mrf, 0);
+        analogWrite(mlb, 0);
+        analogWrite(mlf, 0);
+        Serial.print("calibrating - ");
+        myservo.write(90);
         long duration, cm;
         pinMode(pingPin, OUTPUT);
         digitalWrite(pingPin, LOW);
@@ -22,13 +40,17 @@ void loop() {
         pinMode(echoPin, INPUT);
         duration = pulseIn(echoPin, HIGH);
         cm = microsecondsToCentimeters(duration);
-        Serial.print(cm);
-        Serial.print(" - ");
+        Serial.println(cm);
+        //walldistance = cm;
     }
-    Serial.println("   done 1");
-    for(int i = 5; i>0; i = i-1) {
-        myservo.write(i*36);
-        delay(75);
+    else
+    {
+        analogWrite(mrb, 0);
+        analogWrite(mrf, 255);
+        analogWrite(mlb, 0);
+        analogWrite(mlf, 255);
+        myservo.write(90);
+        delay(50);
         long duration, cm;
         pinMode(pingPin, OUTPUT);
         digitalWrite(pingPin, LOW);
@@ -39,12 +61,84 @@ void loop() {
         pinMode(echoPin, INPUT);
         duration = pulseIn(echoPin, HIGH);
         cm = microsecondsToCentimeters(duration);
+        Serial.println(cm);
+        if (walldistance > cm)
+        {
+            analogWrite(mrb, 0);
+            analogWrite(mrf, 0);
+            analogWrite(mlb, 0);
+            analogWrite(mlf, 0);
+            look(7);
+            delay(1000);
+        }
+    }
+}
+long microsecondsToCentimeters(long microseconds)
+{
+    return microseconds / 29 / 2;
+}
+
+int look(int repeat)
+{
+    int avrageLeft = 0;
+    int avrageRight = 0;
+    for (int i = 0; i < repeat; i++)
+    {
+        myservo.write(0);
+        delay(500);
+        long duration, cm;
+        pinMode(pingPin, OUTPUT);
+        digitalWrite(pingPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(pingPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(pingPin, LOW);
+        pinMode(echoPin, INPUT);
+        duration = pulseIn(echoPin, HIGH);
+        cm = microsecondsToCentimeters(duration);
+        avrageRight += cm;
         Serial.print(cm);
         Serial.print(" - ");
+        myservo.write(180);
+        delay(500);
+        pinMode(pingPin, OUTPUT);
+        digitalWrite(pingPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(pingPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(pingPin, LOW);
+        pinMode(echoPin, INPUT);
+        duration = pulseIn(echoPin, HIGH);
+        cm = microsecondsToCentimeters(duration);
+        avrageLeft += cm;
+        Serial.print(cm);
+        Serial.print(" | ");
     }
-    Serial.println("   done 2");
-   
-}
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+    avrageLeft = avrageLeft / repeat;
+    avrageRight = avrageRight / repeat;
+    Serial.print("avrage: ");
+    Serial.print(avrageLeft);
+    Serial.print(" | ");
+    Serial.print(avrageRight);
+    Serial.println("    complete");
+    myservo.write(90);
+    if (avrageLeft > avrageRight)
+    {
+        analogWrite(mrb, 0);
+        analogWrite(mrf, 0);
+        analogWrite(mlb, 255);
+        analogWrite(mlf, 255);
+        delay(1000);
+        return 2;
+        
+    }
+    else
+    {
+        analogWrite(mrb, 255);
+        analogWrite(mrf, 255);
+        analogWrite(mlb, 0);
+        analogWrite(mlf, 0);
+        delay(1000);
+        return 1;
+    }
 }
